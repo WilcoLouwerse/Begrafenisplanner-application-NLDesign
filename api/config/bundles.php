ac3ae5ec3abf39d67a31f7d4d15e8fc2d1c7c952
Ruben van der Linde <ruben@conduction.nl>
2019-07-23 10:22:46 +0200

    Yarn Security updates on frontend en backend

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -10,5 +10,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+	Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+	Conduction\CommonGroundBundle\CommonGroundBundle::class => ['all' => true],
 ];
