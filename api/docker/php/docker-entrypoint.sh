85598c9fb18f6b0459b55fd7960c471caed7d4e6
Ruben van der Linde <ruben@conduction.nl>
2019-11-11 15:24:24 +0100

    Removed the auto generation of helm charts from entrypoint

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -62,13 +62,14 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		echo "Loading fixtures"
 		bin/console doctrine:fixtures:load  --no-interaction
 		
-		echo "Creating OAS documentation"
-		# Let update the docs to show the latest chages
-		bin/console api:openapi:export --output=/srv/api/public/schema/openapi.yaml --yaml --spec-version=3		
+		# echo "Creating OAS documentation"
+		# Lets update the docs to show the latest chages
+		# bin/console api:openapi:export --output=/srv/api/public/schema/openapi.yaml --yaml --spec-version=3		
 				
-		echo "Updating Helm charts"
-		# Let update the docs to show the latest chages
-		bin/console app:helm:update --location=/srv/api/helm --spec-version=3			
+		# this should only be done in an build		
+		# echo "Updating Helm charts"
+		# Lets update the docs to show the latest chages
+		# bin/console app:helm:update --location=/srv/api/helm --spec-version=3			
 	fi
 fi
 
