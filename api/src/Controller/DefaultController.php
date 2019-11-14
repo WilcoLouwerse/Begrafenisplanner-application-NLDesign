e6968fbb982e326409b5ce037a5f94afd23b09d0
Ruben van der Linde <ruben@conduction.nl>
2019-11-14 12:18:36 +0100

    Setup a front en application version of the proto component

---
+++ api/src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Client;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends AbstractController
+{ 
+	/**
+	* @Route("/")
+ 	* @Template
+	*/
+	public function indexAction(Request $request, EntityManagerInterface $em)
+	{
+		return [];
+	}
+}
