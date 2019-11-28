9eca8175266b97f5802ac78b85a5d50f4b39d113
Ruben van der Linde <rubenvdlinde@gmail.com>
2019-11-28 15:24:32 +0000

    Apply fixes from StyleCI

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -1,24 +1,23 @@
 <?php
+
 // src/Controller/DefaultController.php
+
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
-use GuzzleHttp\Client;
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
-{ 
-	/**
-	* @Route("/")
- 	* @Template
-	*/
-	public function indexAction(Request $request, EntityManagerInterface $em)
-	{
-		return [];
-	}
+{
+    /**
+     * @Route("/")
+     * @Template
+     */
+    public function indexAction(Request $request, EntityManagerInterface $em)
+    {
+        return [];
+    }
 }
