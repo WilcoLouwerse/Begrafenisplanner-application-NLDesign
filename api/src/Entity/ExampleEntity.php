1e09b51a2d6ca17bf882499d0cce099dcaa57992
Ruben van der Linde <ruben@conduction.nl>
2020-01-03 17:23:38 +0100

    removed vng.cloud

--- api/src/Entity/ExampleEntity.php
+++ api/src/Entity/ExampleEntity.php
@@ -11,6 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use Ramsey\Uuid\Uuid;
 
 /**
  * This is an example entity.
@@ -91,10 +92,18 @@ class ExampleEntity
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $camelCase;
-
-    public function getId(): ?int
+    
+    
+    public function getId(): Uuid
     {
-        return $this->id;
+    	return $this->id;
+    }
+    
+    public function setId(Uuid $id): self
+    {
+    	$this->id = $id;
+    	
+    	return $this;
     }
 
     public function getName(): ?string
