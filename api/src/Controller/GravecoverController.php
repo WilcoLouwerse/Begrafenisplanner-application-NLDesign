912263af87e953bfe1fa77af400de23f36de6a9f
Yorick Groeneveld <yorickgroeneveld@hotmail.com>
2020-03-30 16:51:00 +0200

    Added Gravecovercontroller+views, table with first data
    v0.4 
    
    Table still needs loop(now only single item with layout problems), or conversion from json to object. So it can be read to the table. WIP

---
+++ api/src/Controller/GravecoverController.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Service\ApplicationService;
+//use App\Service\RequestService;
+use App\Service\CommonGroundService;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DeveloperController
+ * @package App\Controller
+ * @Route("/grave")
+ */
+class GraveController extends AbstractController
+{
+    private $commonGroundService;
+
+    /**
+     * @Route("/add")
+     * @Template
+     */
+    public function addAction(Session $session, $slug = false, Request $httpRequest, CommonGroundService $commonGroundService, ApplicationService $applicationService)
+    {
+        $this->commonGroundService = $commonGroundService;
+
+        $test = [];
+        $test['testText'] = "Graves :";
+
+        $graves = [];
+        $graves['name'] = "";
+        $graves = $this->commonGroundService->getResourceList($this->commonGroundService->getComponent('grc')['href'].'/graves');
+
+        $test['graves'] = $graves;
+
+        return $test;
+    }
+
+}
