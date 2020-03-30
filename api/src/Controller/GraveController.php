446bfda276422a5cd988ca70ed90add7d42d0865
Wilco Louwerse <wilco@louwerse.com>
2020-03-30 12:32:46 +0200

    First try at connecting with GRC API
    v 0.2

--- api/src/Controller/GraveController.php
+++ api/src/Controller/GraveController.php
@@ -20,6 +20,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class GraveController extends AbstractController
 {
+    private $commonGroundService;
 
     /**
      * @Route("/add")
@@ -27,9 +28,17 @@ class GraveController extends AbstractController
      */
     public function addAction(Session $session, $slug = false, Request $httpRequest, CommonGroundService $commonGroundService, ApplicationService $applicationService)
     {
-        $variables = $applicationService->getVariables();
+        $this->commonGroundService = $commonGroundService;
 
-        return $variables;
+        $test = [];
+        $test['testText'] = "Graves :";
+
+        $graves = [];
+        $graves = $this->commonGroundService->createResource($graves, $this->commonGroundService->getComponent('grc')['href'].'/graves');
+
+        $test['graves'] = $graves;
+
+        return $test;
     }
 
 }
