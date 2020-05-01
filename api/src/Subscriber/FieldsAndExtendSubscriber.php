6355873eab294d89f7629920a30a5698643b0ce1
Robert Zondervan <rjzondervan@gmail.com>
2020-05-01 13:02:26 +0200

    Extension of extends and fields

--- api/src/Subscriber/FieldsAndExtendSubscriber.php
+++ api/src/Subscriber/FieldsAndExtendSubscriber.php
@@ -3,17 +3,15 @@
 namespace App\Subscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
-use App\Service\CommonGroundService;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\Serializer\SerializerInterface;
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
 
 class FieldsAndExtendSubscriber implements EventSubscriberInterface
 {
@@ -54,7 +52,6 @@ class FieldsAndExtendSubscriber implements EventSubscriberInterface
         $authorization = $event->getRequest()->headers->get('Authorization');
 
         $this->commonGroundService->setHeader('Authorization',$authorization);
-
         // Only do somthing if fields is query supplied
         if ((!$fields && !$extends) || $method != 'GET') {
             return $result;
@@ -75,25 +72,39 @@ class FieldsAndExtendSubscriber implements EventSubscriberInterface
                 $contentType = 'application/ld+json';
                 $renderType = 'jsonld';
         }
-
+        $json = $this->serializer->serialize(
+            $result,
+            $renderType,
+            ['enable_max_depth' => true,]
+        );
+        $array = json_decode($json, true);
         if (!is_array($extends)) {
             $extends = explode(',', $extends);
         }
+        foreach($extends as $extend){
+            $extend = explode('.',$extend);
+            $array = $this->recursiveExtend($array, $extend);
+        }
+
+
 
         if($fields != [] && $fields != ""){
             // let turn fields into an array if it isn't one already
             if (!is_array($fields)) {
                 $fields = explode(',', $fields);
+
             }
             // Its possible to nest fields for filterins
             foreach ($fields as $key=>$field) {
                 $field = explode('.',$field);
+                unset($fields[$key]);
                 $field = $this->recursiveField($field);
-                $fields = array_merge($field, $fields);
+
+
+                $fields = $this->array_merge_recursive_ex($fields, $field);
             }
-
+//            die;
             // Overwrite maxdepth for extended properties
-
             // we always need to return an id and links (in order not to break stuff)
             if (!in_array('id', $fields)) {
                 $fields[] = 'id';
@@ -107,9 +118,11 @@ class FieldsAndExtendSubscriber implements EventSubscriberInterface
             if (!in_array('@context', $fields)) {
                 $fields[] = '@context';
             }
+            $array = $this->selectFields($array, $fields);
+
             // now we need to overide the normal subscriber
             $json = $this->serializer->serialize(
-                $result,
+                $array,
                 $renderType,
                 ['enable_max_depth' => true,
                     'attributes'    => $fields, ]
@@ -117,25 +130,11 @@ class FieldsAndExtendSubscriber implements EventSubscriberInterface
         }
         else{
             $json = $this->serializer->serialize(
-                $result,
+                $array,
                 $renderType,
                 ['enable_max_depth' => true,]
             );
         }
-        $array = json_decode($json, true);
-
-        foreach($extends as $extend){
-            $extend = explode('.',$extend);
-            $array = $this->recursiveExtend($array, $extend);
-        }
-
-        $json = $this->serializer->serialize(
-            $array,
-            $renderType,
-            ['enable_max_depth' => true,]
-        );
-
-
         // Creating a response
         $response = new Response(
             $json,
@@ -165,9 +164,45 @@ class FieldsAndExtendSubscriber implements EventSubscriberInterface
         $sub = array_shift($field);
 
         if($field == null)
-            return $sub;
+            return [$sub];
         else
             return [$sub=>$this->recursiveField($field)];
 
     }
+    public function selectFields(array $resource, array $fields){
+        $returnArray = [];
+        foreach($fields as $key=>$field){
+            if(!is_array($field) && key_exists($field, $resource)){
+                $returnArray[$field] = $resource[$field];
+            }
+            elseif(key_exists($key,$resource)){
+                if(!key_exists($key,$returnArray)){
+                    $returnArray[$key] = $this->selectFields($resource[$key],$field);
+                }
+                else{
+                    $returnArray[$key] = array_merge($returnArray[$key],  $this->selectFields($resource[$key],$field));
+                }
+            }
+        }
+        return $returnArray;
+    }
+    public function array_merge_recursive_ex(array $array1, array $array2)
+    {
+        $merged = $array1;
+
+        foreach ($array2 as $key => & $value) {
+            if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+                $merged[$key] = $this->array_merge_recursive_ex($merged[$key], $value);
+            } else if (is_numeric($key)) {
+                if (!in_array($value, $merged)) {
+                    $merged[] = $value;
+                }
+            } else {
+                $merged[$key] = $value;
+            }
+        }
+
+        return $merged;
+    }
+
 }
