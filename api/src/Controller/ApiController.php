1675cbd631e727e4cffecb23a40167540d0aa52d
Robert Zondervan <rjzondervan@gmail.com>
2020-05-14 16:14:17 +0200

    Updates

--- api/src/Controller/ApiController.php
+++ api/src/Controller/ApiController.php
@@ -5,8 +5,11 @@ namespace App\Controller;
 use App\Service\ApiService;
 use Conduction\CommonGroundBundle\Service\CommonGroundService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * The API controller handles all requests that should be redirected to a component for the benefit of AJAX Calls
@@ -29,33 +32,97 @@ class ApiController extends AbstractController
     /**
      * @Route("/{component}/{type}")
      */
-    public function collectionAction($component, $type, Request $request, ApiService $apiService){
+    public function collectionAction($component, $type, Request $request, ApiService $apiService, SerializerInterface $serializer){
+        $contentType = $request->headers->get('accept');
+        if (!$contentType) {
+            $contentType = $request->headers->get('Accept');
+        }
+        switch ($contentType) {
+            case 'application/json':
+                $renderType = 'json';
+                break;
+            case 'application/ld+json':
+                $renderType = 'jsonld';
+                break;
+            case 'application/hal+json':
+                $renderType = 'jsonhal';
+                break;
+            default:
+                $contentType = 'application/json';
+                $renderType = 'json';
+        }
         if($request->isMethod('POST')){
-            return $apiService->createResource(json_decode($request->getContent(),true), $component, $type);
+            $results = $apiService->createResource(json_decode($request->getContent(),true), $component, $type);
         }
         elseif($request->isMethod('GET')){
-            return $apiService->getResourceList($component, $type);
+            $results = $apiService->getResourceList($component, $type);
         }
         else{
-            throw HttpError::METHOD_NOT_ALLOWED;
+            throw new HttpException(405, "METHOD NOT ALLOWED");
         }
+        $response = $serializer->serialize(
+            $results,
+            $renderType,
+            ['enable_max_depth'=> true]
+        );
+
+        // Creating a response
+        $response = new Response(
+            $response,
+            Response::HTTP_OK,
+            ['content-type' => $contentType]
+        );
+        return $response;
     }
     /**
      * @Route("/{component}/{type}/{id}")
      */
-    public function itemAction($component, $type, $id, Request $request, ApiService $apiService){
+    public function itemAction($component, $type, $id, Request $request, ApiService $apiService, SerializerInterface $serializer){
+        $contentType = $request->headers->get('accept');
+        if (!$contentType) {
+            $contentType = $request->headers->get('Accept');
+        }
+        switch ($contentType) {
+            case 'application/json':
+                $renderType = 'json';
+                break;
+            case 'application/ld+json':
+                $renderType = 'jsonld';
+                break;
+            case 'application/hal+json':
+                $renderType = 'jsonhal';
+                break;
+            default:
+                $contentType = 'application/json';
+                $renderType = 'json';
+        }
         if($request->isMethod('PUT')){
-            return $apiService->updateResource(json_decode($request->getContent(),true), $component, $type, $id);
+            $results = $apiService->updateResource(json_decode($request->getContent(),true), $component, $type, $id);
         }
         elseif($request->isMethod('GET')){
-            return $apiService->getResource($component, $type, $id);
+            $results = $apiService->getResource($component, $type, $id);
         }
         elseif($request->isMethod('DELETE')){
-            return $apiService->deleteResource($component, $type, $id);
+            $results = $apiService->deleteResource($component, $type, $id);
         }
-        else{
-            throw HttpError::METHOD_NOT_ALLOWED;
+        else {
+            throw new HttpException(405, "METHOD NOT ALLOWED");
         }
+
+        $response = $serializer->serialize(
+            $results,
+            $renderType,
+            ['enable_max_depth'=> true]
+        );
+
+        // Creating a response
+        $response = new Response(
+            $response,
+            Response::HTTP_OK,
+            ['content-type' => $contentType]
+        );
+
+        return $response;
     }
 
 }
