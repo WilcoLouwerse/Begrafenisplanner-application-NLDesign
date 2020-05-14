993a86a00c6a1e6a6548bea6c6ae91d6a85bbfcf
Robert Zondervan <rjzondervan@gmail.com>
2020-05-14 10:14:30 +0200

    Apic Controller

---
+++ api/src/Controller/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/api", name="api")
+     */
+    public function index()
+    {
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+        ]);
+    }
+}
